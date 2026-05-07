f8e70d469c5fde55c94730bf53bf08f57b0b078e
ISTIAK AHMED BELAYET <mohammadabdullahofc@gmail.com>
2026-05-07 23:29:29 +0600

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  integrations: [tailwind()],
-  outDir: './dist',
-  site: 'https://govtjobbd.pages.dev',
-});
